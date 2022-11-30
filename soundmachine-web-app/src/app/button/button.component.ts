aebd808fb7700dd5fb18eb7691b8746dafc53f19
jekattack <jan.erik.kraus@t-online.de>
2022-11-30 20:59:37 +0100

    loading at rendering

--- soundmachine-web-app/src/app/button/button.component.ts
+++ soundmachine-web-app/src/app/button/button.component.ts
@@ -11,23 +11,22 @@ export class ButtonComponent implements OnInit{
   @Input() emoji!: string;
   @Input() allSounds: string[] = []
   selectedSound: string = '';
-
+  sound = new Audio();
   emojiPath = '';
   soundPath = '';
 
   playSound(){
     console.log("test")
-    let sound = new Audio();
     this.soundPath = "../assets/sounds/" + this.selectedSound + ".mp3";
-    sound.src = this.soundPath;
-    sound.load();
-    sound.play();
+    this.sound.play();
   }
 
   ngOnInit(){
     this.selectedSound = this.emoji;
     this.emojiPath = "../assets/" + this.emoji + ".png";
     this.soundPath = "../assets/sounds/" + this.selectedSound + ".mp3";
+    this.sound.src = this.soundPath;
+    this.sound.load();
   }
 
 }
